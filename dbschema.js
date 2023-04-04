aa4324e33fc5a9dbd4219c9128663b3d8cd28807
Kamil Słomiński <kam_slomka@wp.pl>
2023-04-04 22:30:54 +0200

    Create & Read Data, Express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2023-04-03T20:15:52.249Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
